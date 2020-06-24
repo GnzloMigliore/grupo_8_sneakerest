53c9ffdb633e5e9d97c56d5607b7c3f0c4617869
GonzaloMigliore <gonzalomigliore@gmail.com>
2020-06-24 14:30:33 -0300

    Servidor corriendo en puerto 3000

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+
+
+const controllerWeb = require(path.resolve(__dirname, '..', 'controllers', 'controllersWeb'));
+
+router.get('/', controllerWeb.index);
+router.get('/productos', controllerWeb.productos);
+
+module.exports = router; 
