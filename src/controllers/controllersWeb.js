cd6765769d4acc0878844e0d5337ca4a3d9b9caa
GonzaloMigliore <gonzalomigliore@gmail.com>
2020-07-08 17:20:34 -0300

    arreglo servidor

--- src/controllers/controllersWeb.js
+++ src/controllers/controllersWeb.js
@@ -1,39 +1,13 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-const path = require('path');
-const controllersWeb = {
-    index: function(req,res){
-        res.sendFile(path.resolve(__dirname, '../views/web/index.html'));
-    }
 
-}
-module.exports = controllersWeb;
-=======
-=======
->>>>>>> 636674885a6f186ec0d3a0966d0dca2b7b55b197
-=======
->>>>>>> 636674885a6f186ec0d3a0966d0dca2b7b55b197
-const path = require ('path');
+const path = require('path');
 const fs = require('fs');
 
 
 module.exports = {
   index : function(req,res){
       res.sendFile(path.resolve(__dirname, '..', 'views', 'web', 'index.html'));
-  }
-  ,
-<<<<<<< HEAD
-<<<<<<< HEAD
-  productos: function(req,res){
-      res.sendFile(path.resolve(__dirname, '..', 'views', 'productos', 'productos.html'));
-  }
-}
->>>>>>> master
-=======
-=======
->>>>>>> 636674885a6f186ec0d3a0966d0dca2b7b55b197
-  contacto: function(req,res){
+  },
+    contacto: function(req,res){
     res.sendFile(path.resolve(__dirname, '..', 'views','web','contacto.html'))
 },
   email: function(req,res){
@@ -42,9 +16,4 @@ module.exports = {
   newsletter: function(req,res){
   res.sendFile(path.resolve(__dirname, '..', 'views','web','newsletter.html'))
 }
-<<<<<<< HEAD
 }
->>>>>>> 636674885a6f186ec0d3a0966d0dca2b7b55b197
-=======
-}
->>>>>>> 636674885a6f186ec0d3a0966d0dca2b7b55b197
--- src/routes/web.js
+++ src/routes/web.js
@@ -2,14 +2,8 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-//Requerir el modulo de los controladores
-const webController = require(path.resolve(__dirname, '../controllers/controllersWeb.js'));
-=======
-=======
->>>>>>> 636674885a6f186ec0d3a0966d0dca2b7b55b197
+
+
 
 const controllerWeb = require(path.resolve(__dirname, '..', 'controllers', 'controllersweb'));
 //armo mis rutas
@@ -20,24 +14,3 @@ router.get('/contacto/email', controllerWeb.email);
 router.get('/contacto', controllerWeb.contacto);
 
 module.exports = router; 
-<<<<<<< HEAD
->>>>>>> 636674885a6f186ec0d3a0966d0dca2b7b55b197
-
-
-<<<<<<< HEAD
-module.exports = router;
-=======
-
-const controllerWeb = require(path.resolve(__dirname, '..', 'controllers', 'controllersWeb'));
-
-router.get('/', controllerWeb.index);
-router.get('/productos', controllerWeb.productos);
-
-module.exports = router; 
->>>>>>> master
-=======
->>>>>>> 636674885a6f186ec0d3a0966d0dca2b7b55b197
-=======
-
-
->>>>>>> 636674885a6f186ec0d3a0966d0dca2b7b55b197
