8ba513ab5c26927383d120eff8bf442913d7346d
GonzaloMigliore <gonzalomigliore@gmail.com>
2020-07-21 00:30:10 -0300

    multer

--- src/controllers/controllersAdmin.js
+++ src/controllers/controllersAdmin.js
@@ -9,5 +9,37 @@ module.exports = {
     },
     create: (req, res) => {
         res.render(path.resolve(__dirname, '..','views','admin','create'));
-    }
+    },
+    save: (req,res)=>{
+        let  =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','zapatillas.json')));       
+       let ultimaZapatilla = zapatillas.pop();
+       zapatillas.push(this.ultimaZapatilla);
+       let nuevoProducto={
+        id: ultimaZapatilla.id + 1,
+        marca: req.body.marca,
+        modelo: req.body.modelo,
+        genero: req.genero,
+        color: req.body.color,
+        talle : req.body.talle,
+        precio:req.body.precio,
+        descripcion:req.body.descripcion,
+        imagen : req.filename
+};
+zapatillas.push(nuevoProducto);
+let nuevoProductoGuardar = JSON.stringify(zapatillas,null,2)
+fs.writeFileSync(path.resolve(__dirname,'..','data','zapatillas.json'), nuevoProductoGuardar);
+res.redirect('/admin');
+},
+show: (req,res)=>{
+    let zapatillas =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','zapatillas.json')));
+    //res.send(req.params.id);
+    let miZapatilla;
+    zapatillas.forEach(zapatilla => {
+       if(zapatilla.id == req.params.id){
+           miZapatilla==zapatilla;         
+        }
+    });
+    res.render(path.resolve(__dirname, '..','views','admin','detail'), {miZapatilla})
+
+},
 }
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const multer = require('multer');
 
-
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(__dirname, '..','..','public','images','zaparillas'));
+    },
+    filename: function (req, file, cb) {
+      cb(null, 'zapatilla-'+Date.now() + path.extname(file.originalname));
+    }
+  })
+   
+const upload = multer({ storage });
 
 const controllersAdmin = require(path.resolve(__dirname, '..', 'controllers', 'controllersAdmin'));
 
 router.get('/admin', controllersAdmin.index);
 router.get("/admin/create", controllersAdmin.create);
+router.post("/admin/create", upload.single('imagen'), controllersAdmin.save);
+router.get('/admin/detail/:id', controllersAdmin.show);
 module.exports = router;
