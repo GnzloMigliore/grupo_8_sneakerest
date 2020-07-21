80649fc6fa7020d7e7b8c922fe3a34bf041184df
joaquindl <dominguezlodeiro.joaquin@gmail.com>
2020-07-21 11:26:29 -0300

    Creación partials footer, reparación create y admin

--- src/controllers/controllersAdmin.js
+++ src/controllers/controllersAdmin.js
@@ -1,45 +1,47 @@
 const path = require('path');
 const fs = require('fs');
 
-const zapatillas =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','zapatillas.json')));
+
 
 module.exports = {
-    index: (req,res) =>{     
-        res.render(path.resolve(__dirname, '..', 'views', 'admin', 'administrar'), {zapatillas: zapatillas});
+    index: (req,res) =>{   
+        let zapatillas =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','zapatillas.json')));  
+        res.render(path.resolve(__dirname, '..', 'views', 'admin', 'administrar'), {zapatillas});
     },
     create: (req, res) => {
+        let zapatillas =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','zapatillas.json')));
         res.render(path.resolve(__dirname, '..','views','admin','create'));
     },
     save: (req,res)=>{
-        let  =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','zapatillas.json')));       
-       let ultimaZapatilla = zapatillas.pop();
-       zapatillas.push(this.ultimaZapatilla);
-       let nuevoProducto={
-        id: ultimaZapatilla.id + 1,
-        marca: req.body.marca,
-        modelo: req.body.modelo,
-        genero: req.genero,
-        color: req.body.color,
-        talle : req.body.talle,
-        precio:req.body.precio,
-        descripcion:req.body.descripcion,
-        imagen : req.filename
-};
-zapatillas.push(nuevoProducto);
-let nuevoProductoGuardar = JSON.stringify(zapatillas,null,2)
-fs.writeFileSync(path.resolve(__dirname,'..','data','zapatillas.json'), nuevoProductoGuardar);
-res.redirect('/admin');
-},
-show: (req,res)=>{
-    let zapatillas =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','zapatillas.json')));
-    //res.send(req.params.id);
-    let miZapatilla;
-    zapatillas.forEach(zapatilla => {
-       if(zapatilla.id == req.params.id){
-           miZapatilla==zapatilla;         
-        }
-    });
-    res.render(path.resolve(__dirname, '..','views','admin','detail'), {miZapatilla})
-
-},
+        let zapatillas =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','zapatillas.json')));      
+        let ultimaZapatilla = zapatillas.pop();
+        zapatillas.push(ultimaZapatilla);
+        let nuevoProducto={
+            id: ultimaZapatilla.id+1,
+            marca: req.body.marca,
+            modelo: req.body.modelo,
+            genero: req.genero,
+            color: req.body.color,
+            talle : req.body.talle,
+            precio:req.body.precio,
+            descripcion:req.body.descripcion,
+            imagen : req.filename
+        };
+        zapatillas.push(nuevoProducto);
+        let nuevoProductoGuardar = JSON.stringify(zapatillas,null,2)
+        fs.writeFileSync(path.resolve(__dirname,'..','data','zapatillas.json'), nuevoProductoGuardar);
+        res.redirect('/admin');
+    },
+    show: (req,res)=>{
+        let zapatillas =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','zapatillas.json')));
+        //res.send(req.params.id);
+        let miZapatilla;
+        zapatillas.forEach(zapatilla => {
+            if(zapatilla.id == req.params.id){
+                miZapatilla==zapatilla;         
+            }
+        });
+        res.render(path.resolve(__dirname, '..','views','admin','detail'), {miZapatilla})
+        
+    },
 }
