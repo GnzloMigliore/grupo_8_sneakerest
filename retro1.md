2af22ceade9bbcb930d962bcda33121f2f6537d3
GonzaloMigliore <gonzalomigliore@gmail.com>
2020-07-22 15:18:50 -0300

    multer hecho falta terminar edit

---
+++ retro1.md
@@ -0,0 +1,30 @@
+# Retroespectiva sprint 2
++ ## ___Empezar a implementar___
+    ```
+    Reuniones diarias de media hora para ponernos al día y plantear problemas que tuvimos e intentar solucionarlos en conjunto. Ademas creamos nuevas tareas en Trello.
+___
++ ## ___Hacer más___
+    ```
+Ser mas organizados con respecto a las entregas.
+___
++ ## ___Seguir haciendo___
+    ```
+    Mantenernos unidos y cooperando mutuamente todos con todos. 
+___
++ ## ___Hacer menos___
+    ```
+        Commits. Realizamos muchos commits por demas. 
+___
+
++ ## ___Dejar de hacer___
+    ```
+    Commits de más. 
+
+___
+    
++ ## ___Cuestiones positivas___
+    ```
+       Nos gusto como fue quedando el maquetado del sitio. Los tres colaboramos en conjunto y logramos 
+       tener un sitio navegable, responsivo y con buen diseño.
+
+___
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.resolve(__dirname, '..','..','public','img'));
+      cb(null, path.resolve(__dirname, '..','..','public','images','zapatillas'));
     },
     filename: function (req, file, cb) {
       cb(null, 'zapatilla-'+Date.now() + path.extname(file.originalname));
@@ -20,4 +20,7 @@ router.get('/admin', controllersAdmin.index);
 router.get("/admin/create", controllersAdmin.create);
 router.post("/admin/create", upload.single('imagen'), controllersAdmin.save);
 router.get('/admin/detail/:id', controllersAdmin.show);
+router.get("/delete/:id", controllersAdmin.destroy);
+router.get("/edit/:id", controllersAdmin.edit);
+router.put("/edit/:id", upload.any('imagen'), controllersAdmin.updateZapatillas);
 module.exports = router;
