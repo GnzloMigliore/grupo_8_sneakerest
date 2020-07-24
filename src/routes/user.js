fb766b858e55ba17243971a419df9f11aef28d14
Ezequiel Paolini <paolini.ezequiel@gmail.com>
2020-07-24 14:07:45 -0300

    nav css

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(__dirname, '..','..','public','images','usuarios'));
+    },
+    filename: function (req, file, cb) {
+      cb(null, 'usuario-'+Date.now() + path.extname(file.originalname));
+    }
+  })
+
+  const upload = multer({ storage });
 
 //Requerir el modulo de los controladores
 const controllersUser = require(path.resolve(__dirname, '..', 'controllers', 'controllersUser'));
 
-router.get('/registro', controllersUser.registro);
-router.get('/login', controllersUser.login);
+router.get('/login', controllersUser.index);
+router.get("/login/registro", controllersUser.create);
+router.post("/login/registro", upload.single('imagen'),controllersUser.save);
 router.get('/recover', controllersUser.recover);
 
 module.exports = router;
